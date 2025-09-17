e82ac4d3df1b0807ae2cae6d61d3f0437024c7c1
Ediene26 <ediene.glima@upe.br>
2025-09-16 23:17:17 -0300

    Remove arquivos desnecessarios

--- Livro.java
+++
@@ -1,8 +0,0 @@
-public class Livro {
-    String titulo, autor; 
-    int quantpag;
-    boolean emprestado;
-
-
-    
-}
--- bee_1000.java
+++
@@ -1,9 +0,0 @@
-//Exercício proposto: Hello World!
-
-//Sempre usado para abrir o arquivo criado
-public class bee_1000 {
-    //Ponto de partida do sistema
-    public static void main (String[] args) {
-        System.out.println("Hello World!");
-    }
-}
